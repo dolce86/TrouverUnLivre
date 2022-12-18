989ba5a96125267ef65dce4c5b8d1dedfec5faf7
gwengwendu13 <gwenaellefleury86@gmail.com>
2022-12-18 16:36:39 +0100

    Mon premier commit wesh

---
+++ python_intro.py
@@ -0,0 +1,13 @@
+def hi():
+    
+	if 5 > 2:
+	    print('5 est effectivement plus grand que 2')
+	else:
+	    print("5 n'est pas plus grand que 2")
+
+hi()
+
+
+girls = ['Rachel', 'Monica', 'Phoebe', 'Ola', 'You']
+for name in girls:
+    print('Next girl')
